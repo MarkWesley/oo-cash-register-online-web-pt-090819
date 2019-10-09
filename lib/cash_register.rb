07352ec8cc1aef104261ba0a5f07662cbf58d993
Mark Stone <markwstone85@gmail.com>
2019-10-09 16:43:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,7 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     self.total += (price * quantity)
+    # @last_trans = @total 
     while quantity > 0
       @items << title
       quantity -= 1
@@ -27,8 +28,8 @@ class CashRegister
     @items
   end
 
-  def void_last_transaction(price, quantity=1)
-    @last_trans = price * quantity
+  def void_last_transaction
+    # @last_trans = price * quantity
     @total -= @last_trans
   end
 
